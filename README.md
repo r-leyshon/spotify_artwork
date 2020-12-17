d073c5470245661473770734cd1785315f10f8e6
r-leyshon <richard.leyshon@ons.gov.uk>
2020-12-17 12:10:50 +0000

    retrieving image urls for all albums

--- README.md
+++ README.md
@@ -2,11 +2,11 @@
 
 ## Work in progress.
 
-## Get album artwork for a vector of given albums
+## Get album artwork for a vector of given Spotify album IDs
 
 1. [Configure your application](https://developer.spotify.com/documentation/web-api/). [Help on authorisation](https://developer.spotify.com/documentation/general/guides/authorization-guide/).
 
-2. Create a list of album names (syntax matters) in an xlsx, save to the data folder.
+2. Create a list of album names (syntax matters) in a column called `album_id`, save as .tsv in the data folder.
 
 3. Work from the `.Rproj` file.
 
--- scripts/GET_albums.R
+++ scripts/GET_albums.R
@@ -2,4 +2,35 @@
 query spotify api for album artwork.
 list of albums in data folder.
 "
-library(httr)
+# load packages -----------------------------------------------------------
+library(spotifyr)
+library(magick)
+library(readxl)
+library(rlist)
+
+# source credentials ------------------------------------------------------
+source("credentials/credentials.R")
+
+# get access token --------------------------------------------------------
+# pulled from system variables configured in credentials.R
+access_token <- get_spotify_access_token()
+
+
+# import data ------------------------------------------
+
+album_tracker <- read.delim("data/album_tracker.tsv")
+
+all_albums <- lapply(album_tracker$album_id, get_album)
+
+# select urls -------------------------------------------------------------
+# cbind to dataframe
+album_tracker$img_url <- unlist(list.select(all_albums, images$url[1]))
+
+# query albums ------------------------------------------------------------
+
+# test_album <- get_album("0TkfHTK2sSV4CxrQWqBrNF")
+# myurl <-  test_album[["images"]][["url"]][1]
+# images <- image_read(myurl)
+
+
+
