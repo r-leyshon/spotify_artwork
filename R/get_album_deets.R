4faad67adb6097c8f89c1f09011fa978f1a641b2
r-leyshon <richard.leyshon@ons.gov.uk>
2021-06-07 07:34:42 +0100

    feat: Get album deets messages type of query

--- R/get_album_deets.R
+++ R/get_album_deets.R
@@ -5,9 +5,12 @@ get_album_deets <- function(album_IDs = NULL){
   albums <- lapply(album_IDs, function(album_IDs){
     # if the link is not a url, then query spotify api
     if(!grepl("http", album_IDs)){
+      message("this is a spotify ID")
       get_album(album_IDs)
       # else this iteration must be a url, simply return it
-      } else {return(album_IDs)}
+    } else {
+      message("this is a url")
+      return(album_IDs)}
     })
   
   return(albums)
