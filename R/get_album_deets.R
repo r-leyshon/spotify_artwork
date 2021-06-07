3d0b824c379f2726a7992d4aafd8a398f1a25671
r-leyshon <richard.leyshon@ons.gov.uk>
2021-06-07 07:31:01 +0100

    feat: get_album_deets can now handle direct url

--- R/get_album_deets.R
+++ R/get_album_deets.R
@@ -2,6 +2,13 @@
 
 get_album_deets <- function(album_IDs = NULL){
   # use spotifyr client to get album details
-  albums <- lapply(album_IDs, get_album)
+  albums <- lapply(album_IDs, function(album_IDs){
+    # if the link is not a url, then query spotify api
+    if(!grepl("http", album_IDs)){
+      get_album(album_IDs)
+      # else this iteration must be a url, simply return it
+      } else {return(album_IDs)}
+    })
+  
   return(albums)
 }
